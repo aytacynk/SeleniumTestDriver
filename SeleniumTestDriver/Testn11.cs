dddd0f9717c83b95d428add8e8704310aea3e124
Aytach <aytacyanik@hotmail.com.tr>
2019-01-01 21:49:50 +0300

    Modified commit

--- SeleniumTestDriver/Testn11.cs
+++ SeleniumTestDriver/Testn11.cs
@@ -32,14 +32,14 @@ namespace SeleniumTestDriver
 
             driver.FindElement(By.XPath(".//*[@id='contentListing']/div/div/div[2]/div[3]/a[2]")).Click();
 
-            driver.FindElement(By.XPath(".//*[@id='p-280363057']/div[2]/span")).Click();
+            driver.FindElement(By.XPath(".//*[@id='p-209303029']/div[2]/span")).Click();
 
             driver.FindElement(By.ClassName("myAccount")).Click();
 
             driver.FindElement(By.XPath(".//*[@id='myAccount']/div/div/div[2]/ul/li[6]/a[1]")).Click();
             driver.FindElement(By.XPath(".//*[@id='myAccount']/div[3]/ul/li[1]/div/ul/li/a[1]")).Click();
 
-            driver.FindElement(By.XPath(".//*[@id='p-280363057']/div[3]/span")).Click();
+            driver.FindElement(By.XPath(".//*[@id='p-209303029']/div[3]/span")).Click();
 
             Assert.AreEqual("Test is successfully", Program.createMessage());
         }
